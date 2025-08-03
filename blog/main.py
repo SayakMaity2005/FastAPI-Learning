4431a7ed5bff70123f88982f8f4ab6e9c2606157
Sayak Maity <sayak6481@gmail.com>
2025-08-03 10:36:21 +0530

    Creating a post method using pydantic BaseModel

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Blog(BaseModel):
+    title: str
+    body: str
+
 @app.post("/blogs")
-def create():
-    return "Creating a blog"
+def create(request: Blog):
+    return request
